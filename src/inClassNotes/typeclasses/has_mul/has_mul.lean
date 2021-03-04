c1ebd746ee79e34b00a42e053b163c0a805ef5e2
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-03-04 10:54:21 -0500

    Updates for class on typeclasses

--- src/inClassNotes/typeclasses/has_mul/has_mul.lean
+++ src/inClassNotes/typeclasses/has_mul/has_mul.lean
@@ -1,39 +1,12 @@
-import algebra.group.basic
+universe u
+
+@[class]  
+structure has_mul_ident (α : Type u): Type (u+1) := 
+(mul : α → α → α)
 
 /-
-@[class]
-structure has_mul (α : Type u) := 
-(mul:  α → α → α)
--/
-
-
-#check [has_mul nat]
-#check [has_mul bool]
-
-#check has_mul
-#check has_mul nat
-#check has_mul bool
-
-
-
-namespace hidden
-
-universe u 
-
+Syntactic alternative:
 class has_mul (α : Type u) := (mul : α → α → α)
-
-
-
-/-
-A monoid is a semigroup 
-with an element 1 
-such that 1 * a = a * 1 = a.
-
-@[class]
-structure monoid (M : Type u) : Type u :=
-(mul : M → M → M)
-(mul_assoc : ∀ (a b c_1 : M), (a * b) * c_1 = a * b * c_1)
-(one : M)
-(one_mul : ∀ (a : M), 1 * a = a)
-(mul_one : ∀ (a : M), a * 1 = a)
 -/
+
+
